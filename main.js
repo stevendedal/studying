742d96b4996e7ace082c62401e3e57c3becae93f
Illia <ephemeralmist3@gmail.com>
2022-06-25 02:27:54 +0300

    cart

--- main.js
+++ main.js
@@ -39,11 +39,26 @@ const baseTotalPrice = () => {
     let totalCost = 0;
     base.forEach((cartItem) => {
         totalCost += Number(cartItem.querySelector('.cart_price').innerText) * Number(cartItem.querySelector('.count_cart_curent').innerText);
-        console.log(cartItem.querySelector('.cart_price').innerText);
     });
     document.getElementById('total_price').textContent = totalCost;
 };
-baseTotalPrice();
+   baseTotalPrice();
+
+const subTotalprice = () =>{
+    const Sub = document.querySelectorAll('.item');
+    console.log(document.querySelectorAll('.item'));
+    let subTotalCost = 0;
+    Sub.forEach((element) => {
+        subTotalCost += Number(element.querySelector('.cart_price').innerText) * Number(element.querySelector('.count_cart_curent').innerText);
+        console.log(Number(element.querySelector('.cart_price').innerText));
+        console.log(Number(element.querySelector('.count_cart_curent').innerText));
+        document.querySelector('.subtotal').innerText = subTotalCost;
+       
+}) ;
+
+console.log('subtotal', document.getElementsByClassName('.subtotal').innerText = subTotalCost);
+
+};
 
 const counters = document.querySelectorAll('.cart_count');
 
@@ -62,29 +77,34 @@ counters.forEach((el) => {
 
     if (value >= min) {
         currentValueElement.innerHTML = value;
+        console.log(currentValueElement.innerHTML);
+        subTotalprice();
+        baseTotalPrice();
         
+        
+     
     }
     })
+    
 
   plusButtonEl.addEventListener('click', () => {
     const value = Number(currentValueElement.innerHTML) + 1;
 
     if (value <= max) {
         currentValueElement.innerHTML = value;
+        subTotalprice();
+        baseTotalPrice();
+        
+       
+
     }
   })
 
 })
 
-const subtotal = document.querySelectorAll('.item');
-let totalCost = 0;
-subtotal.forEach((element) => {
-    totalCost += Number(element.querySelector('.cart_price').innerText) * Number(element.querySelector('.count_cart_curent').innerText);
-    console.log(element.querySelector('.count_cart_curent').innerText);
 
-}) ;
-document.getElementsByClassName('.subtotal').textContent = totalCost;
-console.log(document.getElementsByClassName('.subtotal').textContent);
+
+
 
         
 
